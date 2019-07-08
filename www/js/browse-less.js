fd6469097c9f7f18bf43138cf80c21d550f53d85
doppl3r <deben3@gmail.com>
2019-07-08 12:59:12 -0700

    add fade effect on scroll event

--- www/js/browse-less.js
+++ www/js/browse-less.js
@@ -1,4 +1,6 @@
-var options = { active: true };
+let options = { active: true };
+let prev_scroll_position = window.scrollY;
+let ticking = false;
 
 // send data back to chrome extension listener
 function sendOptions(options){
@@ -6,5 +8,23 @@ function sendOptions(options){
         detail: JSON.stringify(options),
     }));
 }
+
+// add scroll listener
+function addScrollListener(){
+    document.addEventListener('scroll', function(e){
+        let next_scroll_position = window.scrollY;
+        let scroll_diff = next_scroll_position - prev_scroll_position;
+        let opacity = document.body.style.opacity;
+        if (opacity == "" || opacity > 1) opacity = 1;
+        prev_scroll_position = next_scroll_position;
+        opacity -= scroll_diff / 1000;
+        document.body.style.opacity = opacity;
+    });
+}
+
+
+
+// instantiate functions
+addScrollListener();
 
 //alert('browse less installed');
