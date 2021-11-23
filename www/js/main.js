e36515ade2690c7b82a7b18f90e9fd3a357ae045
Jacob DeBenedetto <deben3@gmail.com>
2021-11-23 10:09:29 -0700

    Update connection between scripts

--- www/js/main.js
+++ www/js/main.js
@@ -1,29 +1,85 @@
-// Get chrome storage state
-function init(){
-    // Append script if chrome storage state is active
-    chrome.storage.sync.get(null, function(items) {
-        var href = window.location.href;
+(function() {
+    // Define local variables
+    var listHref = [];
+    var listHostname = [];
+    var scrollScale = 2; // TODO: Update from popup message
+
+    function init() {
+        // Update lists from Chrome Storage
+        updateLists();
+
+        // Add listeners
+        addScrollListener();
+        addMessageListener();
+    }
+
+    function updateLists() {
+        // Update lists
+        chrome.storage.sync.get(null, function(items) {
+            listHref = items['href'] || [];
+            listHostname = items['hostname'] || [];
+        });
+    }
+
+    function isBlacklisted() {
+        var href = window.location.href
         var hostname = new URL(href).hostname;
-        var state = "inactive";
-        var listHref = items['href'] || [];
-        var listHostname = items['hostname'] || [];
+        var blacklisted = false;
 
         // Loop through each href list item for matching string
-        listHref.forEach(function(item){ if (item == href) state = "active"; });
-        listHostname.forEach(function(item){ if (item == hostname) state = "active"; });
-
+        listHref.forEach(function(item){ if (item == href) blacklisted = true; });
+        listHostname.forEach(function(item){ if (item == hostname) blacklisted = true; });
+        
         // Add script if any match exists
-        if (state == "active"){ appendScript(); }
-    });
-}
+        return blacklisted;
+    }
 
-// Append browse-less.js to the current page
-function appendScript(){
-    var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('/www/js/browse-less.js');
-    (document.head||document.documentElement).appendChild(s);
-    s.onload = function() { s.remove(); };
-}
+    function addScrollListener() {
+        // Add scroll listener to document
+        document.addEventListener('scroll', checkScroll, true);
+    }
 
-// Initialize application
-init();
+    function checkScroll(e, t) { // e = event, t = target
+        // Check all scrollable elements within the document
+        t = (t == undefined) ? e.target : t;
+        if (t.scrollTop == undefined){
+            t.childNodes.forEach(function(child){
+                // Use recursion to find scrollable child
+                checkScroll(e, child);
+            });
+        }
+        else {
+            let attrName = "data-scroll-top";
+            let prevScrollPosition = t.getAttribute(attrName);
+            let nextScrollPosition = t.scrollTop;
+            let scrollDiff = nextScrollPosition - prevScrollPosition;
+            let opacity = t.style.opacity;
+            if (opacity == "" || opacity > 1) opacity = 1;
+            opacity -= scrollDiff / (t.clientHeight * scrollScale);
+            if (isBlacklisted() == false) opacity = 1; // Reset opacity if not blacklisted
+            t.style.opacity = opacity;
+            t.setAttribute(attrName, t.scrollTop); // Update scroll position attribute
+        }
+    }
+
+    function sendMessageToPopup(message) {
+        // Send message { key: value } from main.js to popup.js
+        chrome.runtime.sendMessage(message, function(response) {
+            console.log(response);
+        });
+    }
+
+    function addMessageListener() {
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
+                console.log(request);
+                if (request.href != null) listHref = request.href;
+                if (request.hostname != null) listHostname = request.hostname;
+                sendResponse({ success: request });
+            }
+        );
+    }
+
+    // Initialize application
+    init();
+})();
--- www/js/popup.js
+++ www/js/popup.js
@@ -1,70 +1,97 @@
-// Initialize elements
-var togglePage = document.getElementById("togglePage");
-var toggleWebsite = document.getElementById("toggleWebsite");
-var review = document.getElementById("review");
+(function() {
+    // Initialize elements
+    var togglePage = document.getElementById("togglePage");
+    var toggleWebsite = document.getElementById("toggleWebsite");
+    var review = document.getElementById("review");
 
-function addFormListeners(){
-    // Add event listener to black list button
-    togglePage.addEventListener("click", function(){ checkListByType('href'); });
-    toggleWebsite.addEventListener("click", function(){ checkListByType('hostname'); });
+    function init() {
+        // Update popup UI
+        updateState('href');
+        updateState('hostname');
 
-    // Add event listener to review link
-    review.addEventListener("click", function(){ 
-        chrome.tabs.create({ url:'https://chrome.google.com/webstore/detail/browse-less/cdbbbaknlcfhdjgnhemhlpemkbnfgoam/reviews?hl=en-US' });
-    });
-}
+        // Add event listeners
+        addFormListeners();
+        addMessageListener();
+    }
 
-function checkListByType(listType) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.storage.sync.get([listType], function(items) {
-            var href = tabs[0].url;
-            var hostname = new URL(href).hostname;
-            var matchIndex = -1;
-            var list = items[listType] || [];
-            var target = listType == 'href' ? href : hostname;
+    function addFormListeners() {
+        // Add event listener to black list button
+        togglePage.addEventListener("click", function(){ checkListByType('href'); });
+        toggleWebsite.addEventListener("click", function(){ checkListByType('hostname'); });
 
-            // Loop through each list item for matching index
-            list.forEach(function(item, index) {
-                if (item == target) matchIndex = index;
-            });
+        // Add event listener to review link
+        review.addEventListener("click", function(){ 
+            chrome.tabs.create({ url:'https://chrome.google.com/webstore/detail/browse-less/cdbbbaknlcfhdjgnhemhlpemkbnfgoam/reviews?hl=en-US' });
+        });
+    }
 
-            // Update list if the target exists in chrome storage
-            if (matchIndex >= 0) { list.splice(matchIndex, 1); }
-            else { list.push(target); }
+    function checkListByType(listType) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.storage.sync.get([listType], function(items) {
+                var href = tabs[0].url;
+                var hostname = new URL(href).hostname;
+                var matchIndex = -1;
+                var list = items[listType] || [];
+                var target = listType == 'href' ? href : hostname;
 
-            // Upload new blacklist
-            chrome.storage.sync.set({[listType]: list}, function(){
-                chrome.tabs.update(tabs[0].id, {url: href});
-                updateState(listType);
+                // Loop through each list item for matching index
+                list.forEach(function(item, index) {
+                    if (item == target) matchIndex = index;
+                });
+
+                // Update list if the target exists in chrome storage
+                if (matchIndex >= 0) { list.splice(matchIndex, 1); }
+                else { list.push(target); }
+
+                // Upload new blacklist
+                chrome.storage.sync.set({[listType]: list}, function(){
+                    updateState(listType);
+                    sendMessageToMain({[listType]: list});
+                });
             });
         });
-    });
-}
+    }
 
-// Set active state of toggle button from chrome storage
-function updateState(listType){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.storage.sync.get([listType], function(items) {
-            var href = tabs[0].url;
-            var hostname = new URL(href).hostname;
-            var state = "inactive";
-            var list = items[listType] || [];
-            var target = listType == 'href' ? href : hostname;
-            var elem = listType == 'href' ? togglePage : toggleWebsite;
+    // Set active state of toggle button from chrome storage
+    function updateState(listType) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.storage.sync.get([listType], function(items) {
+                var href = tabs[0].url;
+                var hostname = new URL(href).hostname;
+                var state = "inactive";
+                var list = items[listType] || [];
+                var target = listType == 'href' ? href : hostname;
+                var elem = listType == 'href' ? togglePage : toggleWebsite;
 
-            // Loop through each list item for matching index
-            list.forEach(function(item){
-                if (item == target) state = "active";
+                // Loop through each list item for matching index
+                list.forEach(function(item){
+                    if (item == target) state = "active";
+                });
+
+                // Update html by state
+                elem.checked = (state == 'active');
             });
-
-            // Update html by state
-            elem.checked = (state == 'active');
         });
-    });
-    
-}
+    }
 
-// Instantiate functions
-addFormListeners();
-updateState('href');
-updateState('hostname');
+    function sendMessageToMain(message) {
+        // Send message { key: value } from popup.js to main.js
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+                console.log(response);
+            });
+        });
+    }
+
+    function addMessageListener() {
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
+                console.log(request);
+                sendResponse({ success: request });
+            }
+        );
+    }
+
+    // Initialize application
+    init();
+})();
